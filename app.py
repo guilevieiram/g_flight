f606f267180cd09e14cabc8b277f31990bc93026
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-08-14 11:00:53 -0300

    changed main to app

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from src.decorators import log
 from src.model import db, flight_model, user_model
 from src.view import messager, ui
 from src.controller import controller
-from main import main
+from main import main as app
 
 if __name__ == "__main__":
-    main(
+    app(
 		controller=controller.FlaskAPIController,
 		user_interface=ui.FlaskUserInterface,
 		messager=messager.TerminalMessager,
