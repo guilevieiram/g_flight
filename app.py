e977e0a2a3824d65bc3ac24869abcd016828d1d0
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-08-14 19:05:09 -0300

    trying to run once more

--- app.py
+++ app.py
@@ -3,35 +3,31 @@ from src.model import db, flight_model, user_model
 from src.view import messager, ui
 from src.controller import controller
 
-def create_app (environ, start_response):
-	_controller = controller.FlaskAPIController
-	_user_interface = ui.FlaskUserInterface
-	_messager = messager.TerminalMessager
-	_flight_model = flight_model.TequilaFlightModel
-	_user_model = user_model.TerminalUserModel
-	_data_base = db.PostgresqlDataBase
+_controller = controller.FlaskAPIController
+_user_interface = ui.FlaskUserInterface
+_messager = messager.TerminalMessager
+_flight_model = flight_model.TequilaFlightModel
+_user_model = user_model.TerminalUserModel
+_data_base = db.PostgresqlDataBase
 
 
-	bot = _controller(
-		port=5000,
-		user_interface=_user_interface(
-			port=4000,
-			backend_endpoint="http://127.0.0.1:5000"
+bot = _controller(
+	port=5000,
+	user_interface=_user_interface(
+		port=4000,
+		backend_endpoint="http://127.0.0.1:5000"
+	),
+	messager=_messager(),
+	flight_model=_flight_model(
+		data_base=_data_base(
+			db_name="flight_data"
+			)
 		),
-		messager=_messager(),
-		flight_model=_flight_model(
-			data_base=_data_base(
-				db_name="flight_data"
-				)
-			),
-		user_model=_user_model(
-			data_base=_data_base(
-				db_name="user_data"
-				)
-			),
-		)
+	user_model=_user_model(
+		data_base=_data_base(
+			db_name="user_data"
+			)
+		),
+	)
 
-	return bot.app
-
-if __name__ == "__main__":
-    app().run()
+app = bot.app
