4134936cfc9b64ecb329c21cb5a508a7fd157d03
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-08-14 18:02:21 -0300

    tried fixing number of arguments

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from src.decorators import log
 from src.model import db, flight_model, user_model
 from src.view import messager, ui
 from src.controller import controller
-from main import main as app
+from main import main
+
+def app (*args, **kwargs):
+	main()
 
 if __name__ == "__main__":
     app()
