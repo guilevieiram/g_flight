c74191e0a999933abfae6f78cbafc7689f68be3b
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-08-18 08:39:39 -0300

    update about page

--- src/view/static/javascript/about.js
+++ src/view/static/javascript/about.js
@@ -2,23 +2,14 @@ function animateAboutParagraph() {
     const paragraphs = document.querySelectorAll('.about-paragraph');
 
     // animate the first
-    gsap.to(paragraphs[0], {
-        y:0,
-        opacity: 1
-    });
-
     paragraphs.forEach((paragraph) => {
-        loadOnReach(
-            element = paragraph,
-            animation = gsap.to,
-            parameters = {
-                y: 0,
-                opacity: 1,
-                duration: 3,
-                ease: 'power2'
-            }
-        );
-    });
+        gsap.to(paragraph, {
+            y: 0,
+            opacity: 1,
+            duration: 3,
+            ease: 'power2'
+        });
+    })
 }
 
 animateAboutParagraph();
